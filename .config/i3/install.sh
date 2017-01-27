03f1833a6b9f26e771f25157b0c0ace506fc5bdd
Rishi Goomar <rishig92@gmail.com>
2017-01-27 13:18:32 -0600

    Add playerctl dependency

--- .config/i3/install.sh
+++ .config/i3/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Utilities that this i3 config uses
-sudo apt-get install -y i3 dunst volti blueman-applet scrot xbacklight
+sudo apt-get install -y i3 dunst volti blueman-applet scrot xbacklight playerctl
